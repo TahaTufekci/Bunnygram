dc177f0f510bd3ce49ce78758bc26c7856dfd480
TahaTüfekçi <57589245+TahaTufekci@users.noreply.github.com>
2022-08-29 09:55:26 +0300

    Bunnygram

---
+++ nonogram-game2/Assets/Scripts/Models/Controllers/ButtonController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ButtonState{
+    Opened = 0,
+    Closed = 1
+}
+
+namespace Nonogram
+{
+    public class ButtonController : MonoBehaviour
+{
+
+    [SerializeField] private Button button;
+    
+
+    public ButtonState buttonState;
+ 
+
+    public void SetButtons(){
+        button.onClick.AddListener(ChangeButtonSprite);
+    }
+
+    public void ChangeButtonSprite(){
+        if(buttonState == ButtonState.Opened){
+            button.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            buttonState = ButtonState.Closed;
+        }
+        else{
+            button.gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            buttonState = ButtonState.Opened;
+        }
+
+    }
+
+    private void SetState(ButtonState state){
+        buttonState = state;
+    }
+    public ButtonState GetState(){
+        return buttonState;
+    }   
+}
+}
+
